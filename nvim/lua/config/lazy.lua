97c36fe9fdfa525b5001d8b86696496be6387e6d
kouya takahashi <3kamarin4skaya6@gmail.com>
2026-01-10 12:56:28 +0900

    fix plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -27,6 +27,10 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+  ui = {
+      border = "rounded",
+  },
+  debug = false,
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
