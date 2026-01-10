fd5c4374303624d379b1baeb3ab3d40620d263e5
kouya takahashi <3kamarin4skaya6@gmail.com>
2026-01-10 13:31:36 +0900

    add win shell

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,12 @@ require("config.lazy")
 require("config.options")
 require("config.keymaps")
 
+
+
+-- Windows: use PowerShell as default shell
+if vim.fn.has("win32") == 1 then
+    vim.opt.shell = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+end
