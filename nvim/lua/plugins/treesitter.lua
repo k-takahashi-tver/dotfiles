5b67b2a149b7457d5a2a91b5db8198ef9ae616fe
kouya takahashi <3kamarin4skaya6@gmail.com>
2026-01-10 13:32:24 +0900

    fix

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,33 +2,25 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        auto_install = true,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            "windwp/nvim-ts-autotag",
-        },
-        main = "nvim-treesitter.configs",
-        ensure_installed = {
-            "lua", "vim", "markdown", "html", "bash", "python", "rust", "yaml", "json", "sql", "markdown_inline", "hcl"
-        },
         opts = {
-            highlight = { enable = true },
-        --    autotag = { enable = true },
+          ensure_installed = {
+            "lua", "vim", "vimdoc",
+            "python", "rust",
+            "bash", "json", "yaml", "markdown", "markdown_inline",
+            "html", "sql",
+          },
+          auto_install = true,
+          highlight = { enable = true },
         },
-
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
-        event = "CursorMoved",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
     },
     {
         "windwp/nvim-ts-autotag",
-         -- event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-         -- event = "LazyFile",
-         opts = {},
-         config = function()
-             require("nvim-ts-autotag").setup()
-         end,
-     },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {},
+    },
 }
 
