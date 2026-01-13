91ef5c0ea0db8f30c7d6ee069e7e6b7e06dde9d7
kouya takahashi <3kamarin4skaya6@gmail.com>
2026-01-14 06:57:07 +0900

    add mapleader config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,22 @@
+-- vim.cmd.colorscheme("tokyonight")
+
+-- lua ファイルのコメント文字色を調整
+vim.api.nvim_set_hl(0, "Comment", {
+    fg = "#8b949e", -- 明るめグレー
+    italic = true,
+})
+
+-- netrm を無効化（起動時の変な挙動をなくす）
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- leader を Space に割り当てる
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
-
 
 
 -- Windows: use PowerShell as default shell
